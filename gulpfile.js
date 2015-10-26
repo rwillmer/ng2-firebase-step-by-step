171eaf1681ebd629fd26fa44352235872d1803b6
Rachel Willmer <rachel@willmer.org>
2015-10-26 16:30:50 +0000

    Working test. Step 1 Complete.

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ gulp.task('e2e-test', function() {
     gulp.src(protractor_conf.config.specs)
         .pipe(protractor({
             configFile: "e2e-tests/protractor.conf.js",
-            args: ['--baseUrl', 'http://127.0.0.1:8000']
+            args: ['--baseUrl', 'http://127.0.0.1:3000']
         }))
         .on('error', function(e) {
           throw e
