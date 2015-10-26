c781114ef18de01bd96cf8f6cb4c0e5397e973a7
Rachel Willmer <rachel@willmer.org>
2015-10-26 16:16:32 +0000

    start/stop webserver in e2e-test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var protractor = require("gulp-protractor").protractor;
 var protractor_conf = require('./e2e-tests/protractor.conf');
+var browserSync = require('browser-sync');
 
 gulp.task('default', function() {
   // place code for your default task here
@@ -8,11 +9,35 @@ gulp.task('default', function() {
 
 gulp.task('e2e-test', function() {
 
+    var bs = browserSync.create();
+    bs.init({
+        server: {
+            baseDir: "src/client"
+        },
+        open: false
+    });
+
     gulp.src(protractor_conf.config.specs)
         .pipe(protractor({
             configFile: "e2e-tests/protractor.conf.js",
             args: ['--baseUrl', 'http://127.0.0.1:8000']
         }))
-        .on('error', function(e) { throw e });
+        .on('error', function(e) {
+          throw e
+        })
+        .on('end', function () {
+          // Close browser sync server
+          bs.exit();
+        })
 
+});
+
+gulp.task('serve-dev', function() {
+
+  /**
+   * Run Browsersync with server config
+   */
+  browserSync({
+      server: "src/client",
+  });
 });
