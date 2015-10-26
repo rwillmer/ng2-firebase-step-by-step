1b29c7c236f9d62308dec7b584dfa83809f261dd
Rachel Willmer <rachel@willmer.org>
2015-10-26 16:09:36 +0000

    Add protractor to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var protractor = require("gulp-protractor").protractor;
+var protractor_conf = require('./e2e-tests/protractor.conf');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('e2e-test', function() {
+
+    gulp.src(protractor_conf.config.specs)
+        .pipe(protractor({
+            configFile: "e2e-tests/protractor.conf.js",
+            args: ['--baseUrl', 'http://127.0.0.1:8000']
+        }))
+        .on('error', function(e) { throw e });
+
 });
