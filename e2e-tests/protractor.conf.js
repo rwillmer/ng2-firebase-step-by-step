f4b5c96b056529e9f66239fcb1aaa3772f7fb581
Rachel Willmer <rachel@willmer.org>
2015-10-28 21:29:22 +0000

    Add useAllAngular2AppRoots option

--- e2e-tests/protractor.conf.js
+++ e2e-tests/protractor.conf.js
@@ -9,6 +9,12 @@ exports.config = {
     '../e2e-tests/tests/**/*.spec.js'
   ],
 
+  // Special option for Angular2, to test against all Angular2 applications
+  // on the page. This means that Protractor will wait for every app to be
+  // stable before each action, and search within all apps when finding
+  // elements.
+  useAllAngular2AppRoots: true,
+
   // allow for testing of non-Angular sites
   //
   // From http://ng-learn.org/2014/02/Protractor_Testing_With_Angular_And_Non_Angular_Sites/
